cdd9ba8177ef9d2216d63596eacc89b85369173a
Byohazyrd <benjaminmikkelson@gmail.com>
2021-02-09 08:49:27 -0700

    added remaining changes from previous commit

--- serialization.rb
+++ serialization.rb
@@ -1,6 +1,5 @@
 require "yaml"
 
-
 module Serialization
 
   def get_word
@@ -8,7 +7,6 @@ module Serialization
     local_file = File.open('5desk.txt', 'r')
     while !local_file.eof?
       line = local_file.readline
-      #puts line
       unless line.length < 5 || line.length > 12
         words << line
       end
@@ -17,31 +15,20 @@ module Serialization
     return words.sample.downcase.chomp
   end
   
-  #SAVE_GAME
   def save_game(game_instance)
-    #first, determine if there is an output directory, if not, make one
-    #make a file that can be used to write the contents of the game_instance
-    #write the contents 
-    #close the file
     
+    #makes a directory for the saves if it doesn't exist
     Dir.mkdir('saved_games') unless Dir.exists?('saved_games')
 
+    #saves the game using the player name and an input string
     puts "What you would like name your game?"
     input = gets.chomp
-
     filename = "saved_games/hangman_#{game_instance.player_name}_#{input}.txt"
-    
-    #filename = "saved_games/hangman_#{game_instance.player_name}.txt"
-
     serialized_game = YAML.dump(game_instance)
-
     File.open(filename, 'w') do |file|
       file.puts serialized_game
     end
-
     puts "Your game was saved as #{filename}"
   end
-
-  #LOAD_GAME
 
 end
